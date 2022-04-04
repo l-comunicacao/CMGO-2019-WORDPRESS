b1543be5e07dca7a5b1dc0279c6f3f29ec2ee330
jammal767 <bruno@lcomunicacao.com>
2022-04-03 22:01:23 -0300

    Teste

--- _wp-config.php
+++ _wp-config.php
@@ -21,7 +21,7 @@
 // ** Configurações do MySQL - Você pode pegar estas informações
 // com o serviço de hospedagem ** //
 /** O nome do banco de dados do WordPress */
-define('DB_NAME', 'cbrh');
+define('DB_NAME', 'cbrh'); 
 
 /** Usuário do banco de dados MySQL */
 define('DB_USER', 'root');
